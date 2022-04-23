ad13a62a4e4591d3ad9c402a5668a59edfd38bca
mzulfi1 <98135426+mzulfi1@users.noreply.github.com>
2022-04-23 21:32:39 +0700

    Add files via upload

---
+++ UTP/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style>
+		.bg-hero{
+			background-color: #0dcaf0;
+		}
+	</style>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="#">Muhammad Zulfikar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href"index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="jadwal.php">Jadwal Kuliah</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br><br>
+<section id="hero" class="pt-5 pb-5 bg-hero" >
+	<div class="container">
+		<div class="row flex-md-row-rev">
+			<div class="col-2">
+				<img src="FOTO DIRI.jpg" class="img-fluid">
+			</div>
+			<div class="col-6">
+				<h1>Selamat Datang</h1>
+				<div class="mt-4 mb-4">
+					Perkenalkan saya Muhammad Zulfikar NPM 2017051052 Kelas B <br>
+					Hobi: bermain game, membaca komik, dan menonton film 
+				</div>
+				<button type="button" class="btn btn-outline-primary">
+					<a href="https://www.instagram.com/m.zulfi0908/">Instagram</a>
+				</button>
+			</div>
+		</div>
+	</div>
+</section>
+</body>
+</html>
---
+++ UTP/jadwal.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="#">Muhammad Zulfikar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="jadwal.php">Jadwal Kuliah</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<table class="table table-bordered mt-5">
+  <thead>
+	<tr>
+	  <th colspan="6" class="table-danger">JADWAL STANDAR</th>
+	<tr>
+    <tr>
+      <th scope="col" class="table-dark">Waktu</th>
+      <th scope="col" class="table-dark">Senin</th>
+      <th scope="col" class="table-dark">Selasa</th>
+      <th scope="col" class="table-dark">Rabu</th>
+	  <th scope="col" class="table-dark">Kamis</th>
+	  <th scope="col" class="table-dark">Jumat</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">07.30-09.10</th>
+      <td class="table-secondary">-</td>
+      <td class="table-secondary">-</td>
+      <td class="table-info">ADSI</td>
+	  <td class="table-secondary">-</td>
+	  <td class="table-warning">IOT(P)</td>
+    </tr>
+    <tr>
+      <th scope="row">09.20-11.00</th>
+      <td class="table-warning">TAM(P)</td>
+      <td class="table-secondary">-</td>
+      <td class="table-info">TI</td>
+	  <td class="table-info">ML</td>
+	  <td class="table-secondary">-</td>
+    </tr>
+    <tr>
+      <th scope="row">11.10-12.50</th>
+	  <td class="table-warning">ADSI(P)</td>
+	  <td class="table-info">ML</td>
+      <td class="table-secondary">-</td>
+      <td class="table-warning">WEB(P)</td>
+	  <td class="table-secondary">-</td>
+    </tr>
+	<tr>
+      <th scope="row">13.30-15.10</th>
+	  <td class="table-info">IOT</td>
+	  <td class="table-info">WEB</td>
+      <td class="table-secondary">-</td>
+      <td class="table-info">AI</td>
+	  <td class="table-secondary">-</td>
+    </tr>
+	<tr>
+      <th scope="row">15.30-17.00</th>
+	  <td class="table-info">PDT</td>
+	  <td class="table-info">AI</td>
+      <td class="table-info">TAM</td>
+      <td class="table-warning">PDT(P)</td>
+	  <td class="table-secondary">-</td>
+    </tr>
+  </tbody>
+</table>
+<table class="table table-bordered">
+  <thead>
+	<tr>
+	  <th colspan="6" class="table-danger">JADWAL RAMADHAN</th>
+	<tr>
+    <tr>
+      <th scope="col" class="table-dark">Waktu</th>
+      <th scope="col" class="table-dark">Senin</th>
+      <th scope="col" class="table-dark">Selasa</th>
+      <th scope="col" class="table-dark">Rabu</th>
+	  <th scope="col" class="table-dark">Kamis</th>
+	  <th scope="col" class="table-dark">Jumat</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">07.30-08.40</th>
+      <td class="table-secondary">-</td>
+      <td class="table-secondary">-</td>
+      <td class="table-info">ADSI</td>
+	  <td class="table-secondary">-</td>
+	  <td class="table-warning">IOT(P)</td>
+    </tr>
+    <tr>
+      <th scope="row">08.50-10.00</th>
+      <td class="table-warning">TAM(P)</td>
+      <td class="table-secondary">-</td>
+      <td class="table-info">TI</td>
+	  <td class="table-info">ML</td>
+	  <td class="table-secondary">-</td>
+    </tr>
+    <tr>
+      <th scope="row">10.10-11.20</th>
+	  <td class="table-warning">ADSI(P)</td>
+	  <td class="table-info">ML</td>
+      <td class="table-secondary">-</td>
+      <td class="table-warning">WEB(P)</td>
+	  <td class="table-secondary">-</td>
+    </tr>
+	<tr>
+      <th scope="row">12.30-13.40</th>
+	  <td class="table-info">IOT</td>
+	  <td class="table-info">WEB</td>
+      <td class="table-secondary">-</td>
+      <td class="table-info">AI</td>
+	  <td class="table-secondary">-</td>
+    </tr>
+	<tr>
+      <th scope="row">13.50-15.00</th>
+	  <td class="table-info">PDT</td>
+	  <td class="table-info">AI</td>
+      <td class="table-info">TAM</td>
+      <td class="table-warning">PDT(P)</td>
+	  <td class="table-secondary">-</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="table table-sm">
+  <tbody>
+    <tr>
+      <td class="table-info"></td>
+      <td>Teori</td>
+    </tr>
+    <tr>
+       <td class="table-warning"></td>
+      <td>Praktikum</td>
+    </tr>
+    <tr>
+      <td class="table-secondary"></td>
+      <td>Kosong</td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
